90c14dff9c9cf845d319e78ef7f6feb8f64e9411
Tanya <tatiana,orlyanskaya@gmail.com>
2021-09-28 20:34:40 +0300

    hw9

---
+++ hw9/hw9.js
@@ -0,0 +1,25 @@
+function randomGame(number, random){ 
+  random = Math.floor((Math.random() * 100)+1); 
+    while (number!== null) { 
+      number = prompt('ВВЕДИТЕ ЧИСЛО ОТ 1 ДО 100',''); 
+      if (number <1 || number >100)
+        console.log('не валидное число')
+      else if(number > random) 
+        console.log("Много!"); 
+      else if (number < random) 
+        console.log("Мало!"); 
+      else if (number == random) { 
+        console.log("Правильно!"); 
+        break; 
+      } 
+    } 
+  }; 
+    console.log(randomGame());
+    while(true){
+    const gameAsk = confirm('сыграть еще раз?');
+    if (gameAsk) {
+      console.log(randomGame());
+    } else {
+      break
+    }
+  };
