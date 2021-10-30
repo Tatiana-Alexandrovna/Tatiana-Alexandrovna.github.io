d193bdab515bc02b63fe89245555cd890f07ad5d
Tanya <tatiana,orlyanskaya@gmail.com>
2021-10-30 18:51:40 +0300

    hw18

--- hw18/hw18.js
+++ hw18/hw18.js
@@ -3,17 +3,17 @@ const tr = table.querySelectorAll('.row');
 
 
 function sortTable(columnIndex) {
+    let index = columnIndex;
     let array = Array.from(tr);
     let sortTable = array
         .slice(1)
         .sort((row1, row2) =>
-            (row1.querySelectorAll('td')[columnIndex].innerHTML > row2.querySelectorAll('td')[columnIndex].innerHTML) ? 1 : -1);
+            (row1.querySelectorAll('td')[index].innerHTML > row2.querySelectorAll('td')[index].innerHTML) ? 1 : -1);
     sortTable.forEach(row => {
         table.append(row);
     });  
-}
 
-sortTable(0);
+
 
 
 
